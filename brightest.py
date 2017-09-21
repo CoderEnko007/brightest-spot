318d03ec73123af26938dfac3b4d994f841b3d33
CoderEnko007 <30767128+CoderEnko007@users.noreply.github.com>
2017-09-21 17:31:52 +0800

    dectect brightest spot

---
+++ brightest.py
@@ -0,0 +1,24 @@
+import numpy as np
+import argparse
+import cv2
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required = True)
+ap.add_argument("-r", "--radius", type = int)
+args = vars(ap.parse_args())
+
+image = cv2.imread(args["image"])
+orig = image.copy()
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+
+(minVal, maxVal, minLoc, maxLoc) = cv2.minMaxLoc(gray)
+cv2.circle(image, maxLoc, 5, (255, 0, 0), 2)
+cv2.imshow("Native", image)
+
+gray = cv2.GaussianBlur(gray, (args["radius"], args["radius"]), 0)
+(minVal, maxVal, minLoc, maxLoc) = cv2.minMaxLoc(gray)
+image = orig.copy()
+cv2.circle(image, maxLoc, args["radius"], (255, 0, 0), 2)
+cv2.imshow("Robust", image)
+cv2.waitKey(0)
